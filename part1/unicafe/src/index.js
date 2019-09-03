d91f7ce9270c9e275ae1162d1375723c30162d8c
Alexander Maja <alex-maja@hotmail.com>
2019-09-03 19:34:59 +0300

    Unicafe exercise with full functionality

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,81 +7,65 @@ const Header = (props) => (
     </h1>
 )
 
+// Renders a button with an onClick function and a text from props
 const Button  = (props) => (
     <button onClick={props.handleClick}>
         {props.text}
     </button>
 )
 
-
 const Statistic = (props) => {
-    if(props.type === "positive"){
-        return(
-            <tr>
+    return(
+        <tr>
             <td>{props.text}</td>
-            <td>{props.value}%</td>
+            <td>{props.value}</td>
         </tr>
-        )
-    } else {
-        return(
-            <tr>
-                <td>{props.text}</td>
-                <td>{props.value}</td>
-            </tr>
-        )
-    }
-}
-
-const Statistics = (props) => {
-    const average = props.total ? ((props.good - props.bad) / props.total) : 0
-    const positive = props.total ? (props.good / props.total* 100) : 0
-    return  (
-        <div>
-            <table>
-                <tbody>
-                <Statistic text="Good" value={props.good}/>
-            <Statistic text="Neutral" value={props.neutral}/>
-            <Statistic text="Bad" value={props.bad}/>
-            <Statistic text="Total" value={props.total} />
-            <Statistic text="Average" value={average} />
-            <Statistic text="Positive" value={positive} type="positive"/>
-                </tbody>
-            </table>
-        </div>
     )
 }
 
+// Renders all statistic components
+const Statistics = (props) => {
+    if(props.total !== 0){
+        const average = (props.good - props.bad) / props.total
+        const positive = (props.good / props.total* 100) + "%"
+        return  (
+            <div>
+                <table>
+                    <tbody>
+                    <Statistic text="Good" value={props.good}/>
+                    <Statistic text="Neutral" value={props.neutral}/>
+                    <Statistic text="Bad" value={props.bad}/>
+                    <Statistic text="Total" value={props.total} />
+                    <Statistic text="Average" value={average} />
+                    <Statistic text="Positive" value={positive}/>
+                    </tbody>
+                </table>
+            </div>
+        )
+    } else {
+        return <div><p>No feedback given</p></div>
+    }
+    
+}
+
 const App = () => {
-    const title = "Give feedback"
-    const goodT = "Good"
-    const neutralT = "Neutral"
-    const badT = "Bad"
-    // save clicks of each button to own state
+    // Save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
     const [total, setTotal] = useState(0)
 
-    const addToGood = newValue => {
-        setGood(newValue)
-        addToTotal(total + 1)
+    const addValue = (state, newValue) =>{
+        state(newValue)
+        setTotal(total+1)
     }
-    const addToNeutral = newValue => {
-        setNeutral(newValue)
-        addToTotal(total + 1)
-    }
-    const addToBad = newValue => {
-        setBad(newValue)
-        addToTotal(total + 1)
-    }    
-    const addToTotal = newValue =>setTotal(newValue)
    
     return (
     <div>
-        <Header text={title} />
-        <Button handleClick={() => addToGood(good + 1)} text={goodT} />
-        <Button handleClick={() => addToNeutral(neutral + 1)} text={neutralT} />
-        <Button handleClick={() => addToBad(bad + 1)} text={badT} />
+        <Header text="Give feedback" />
+        <Button handleClick={() => addValue(setGood, good + 1)} text="Good" />
+        <Button handleClick={() => addValue(setNeutral, neutral + 1)} text="Neutral" />
+        <Button handleClick={() => addValue(setBad, bad + 1)} text="Bad" />        
         <Header text="Statistics" />
         <Statistics total={total} good={good} bad={bad} neutral={neutral}/>
     </div>
