f54d1ba02835a797135f808931a9e70d6f7c35cd
Alexander Maja <alex-maja@hotmail.com>
2019-09-06 01:54:50 +0300

    An attempt at Exercise 2.13, not functional and currently commented out
    Country has a weather component, with is supposed to make a database request (which it does) and then draw a <div> based on the information it gets, but for some reason it does not draw anything

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import Countries from './components/Countries'
 
 function App() {
-  require('dotenv').config()
   const [newFilter, setNewFilter] = useState('')
   const [newCountries, setNewCountries] = useState([])
   const handleAddFilter = (event) => setNewFilter(event.target.value)
--- Part2/countries/src/components/Country.js
+++ Part2/countries/src/components/Country.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import axios from 'axios'
 
 const Country = (props) => {
     const country = props.country
     console.log('Country', country)
-    return (<div>
+    return (
+        <div>
+            <Info country={country} />
+            <Languages country={country} />
+            {/*<Weather capital={country.capital} />*/}
+        </div>
+    )
+}
+
+const Info = (props) => {
+    const country = props.country
+    return (
         <div>
             <h2>{country.name}</h2>
             <p>Capital: {country.capital}</p>
             <p>Population: {country.population}</p>
         </div>
+    )
+}
+const Languages = (props) => {
+    const country = props.country
+    return (
         <div>
             <h3>Languages</h3>
             <ul>
@@ -16,9 +33,49 @@ const Country = (props) => {
                     return <li key={lang.iso639_2}>{lang.name}</li>;
                 })}
             </ul>
-            <img src={country.flag} tag="Country flag" width={500} height={300} mode='fit' border="1" alt={country.name + " flag"}/>
+            <img src={country.flag} tag="Country flag" width={500} height={300} mode='fit' border="1" alt={country.name + " flag"} />
         </div>
-    </div>);
+    )
 }
+
+/*
+const Weather = (props) => {
+    // Need to somehow incorporate useState to get this to function? Currently does not render due to missing things to render
+    function draw({props}){
+        useEffect(() => {
+            const api_key = "e384ad3cf8dc402ca0b205505190509&q"
+            const url = "https://api.apixu.com/v1/current.json?key=" + api_key + "=" + props.capital
+            console.log('url', url)
+            const buildWeather = (data) => {
+                console.log('data', data)
+                return (
+                    <div>
+                        <h3>Weather in {props.capital}</h3>
+                        <strong>Temperature: </strong> {data.temp_c}
+                        <img src={data.condition.icon} height="64" width="64" alt={props.capital + "Weather"}></img>
+                        <strong>Wind speed:</strong> {data.wind_kph} <strong>direction:</strong> {data.wind_dir}
+                    </div>
+                )
+            }
+            console.log('Making a database request')
+            axios
+                .get(url).then((response) => {
+                    console.log('responswe', response)
+                    return (
+                        buildWeather(response.data.current)
+                    )
+                })
+        }, [])
+    }
+    
+
+    return (
+        <div>
+            {draw}
+        </div>
+
+    )
+
+}*/
 
 export default Country
