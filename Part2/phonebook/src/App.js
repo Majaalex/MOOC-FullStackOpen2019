9e4c1df810fc1d18683bcdefa40d910d7bb63972
Alexander Maja <alex-maja@hotmail.com>
2019-09-05 21:30:07 +0300

    Exercise 2.11, functional
    the persons useState is now loaded from db.json

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Person from './components/Person'
 import Filter from './components/Filter'
 import Form from './components/Form'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
@@ -42,6 +39,16 @@ const App = () => {
       ? true
       : false
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
   return (
     <div>
       <h1>Phonebook</h1>
