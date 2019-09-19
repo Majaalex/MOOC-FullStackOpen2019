a9a3a09fb1feebe39442d2b06089f9ca5a8e106e
Alexander Maja <alex-maja@hotmail.com>
2019-09-19 18:39:04 +0300

    Exercise 3.9-3.11 support

--- Part2/phonebook/src/services/persons.js
+++ Part2/phonebook/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = "/api/persons"
+const baseUrlLocal = 'https://salty-hollows-94670.herokuapp.com/api/persons'
 const infoUrl = 'http://localhost:3001/info'
 
 const getAll = () => {
