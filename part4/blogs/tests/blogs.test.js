58d78cb0418bbb07d3f593c57d8cb04cfb3da60a
Alexander Maja <alex-maja@hotmail.com>
2019-09-22 15:21:50 +0300

    Exercise 4.4

--- part4/blogs/tests/blogs.test.js
+++ part4/blogs/tests/blogs.test.js
@@ -1,7 +1,48 @@
 const listHelper = require('../utils/list_helper')
-test('Dummy returns one',() => {
+test('Dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('Total likes', () => {
+  const listWithOneBlog = [
+    {
+      title: '5 planning principles for agile development',
+      author: 'Isaac Sacolick',
+      url: 'https://www.infoworld.com/article/3438726/5-planning-principles-for-agile-development.html',
+      likes: 16,
+      id: '5d8751a3c38e831c1c62d7ef'
+    }
+  ]
+  const listWithNoBlogs = []
+  const listWithTwoBlogs = [
+    {
+      title: '5 planning principles for agile development',
+      author: 'Isaac Sacolick',
+      url: 'https://www.infoworld.com/article/3438726/5-planning-principles-for-agile-development.html',
+      likes: 16,
+      id: '5d8751a3c38e831c1c62d7ef'
+    },
+    {
+      title: 'Javascript debugging [slightly] beyond console.log',
+      author: 'Swizec Teller',
+      url: 'https://swizec.com/blog/javascript-debugging-slightly-beyond-console-log/swizec/6633',
+      likes: 18,
+      id: '5d8751f9c38e831c1c62d7f0'
+    }
+  ]
+  test('Of empty list is zero', () => {
+    const result = listHelper.totalLikes(listWithNoBlogs)
+    expect(result).toBe(0)
+  })
+  test('When list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(16)
+  })
+  test('Of a bigger list is calculated correctly', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    expect(result).toBe(34)
+  })
+})// Describe end
--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -5,6 +5,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+  if (blogs.length === 1){
+    return blogs[0].likes
+  }
+  if (blogs.length > 1){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    const mappedLikes = blogs.map(b => b.likes)
+    const totalLikes = mappedLikes.reduce(reducer)
+    return totalLikes
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
